40a2c635ec96a69b87899fab489764136a5869ea
martijn van de ree <mrtjnvanderee@gmail.com>
2021-03-24 12:29:58 +0100

    lighthouse 100% score

--- out/_next/static/8dDtOnCXxX1Yw_GcS4R6J/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-001d9b9a39424b26a23a.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-497a0e7f9aeb51433ac2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- out/_next/static/H1nFdQYaEYXl69nQJbKu6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-8e4dee2dda6c740678ae.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-497a0e7f9aeb51433ac2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
